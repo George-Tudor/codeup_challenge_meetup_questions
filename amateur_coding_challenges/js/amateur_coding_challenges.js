669b4a72ebdb4c291fc60b668adabc43cfcb197e
George Tudor <Alexander.George.Tudor@gmail.com>
2022-01-14 23:32:42 -0600

    problem 4 of amateur coding challenges from Codeup appendix

--- amateur_coding_challenges/js/amateur_coding_challenges.js
+++ amateur_coding_challenges/js/amateur_coding_challenges.js
@@ -26,7 +26,7 @@ console.log(sumOfSequence(4, 10, 5));
 
 
 
-// Given a string of any length with any characters in it, write a function to determine whether or not the string contains the whole word "english". The order of characters/spelling is important, a string "agkjnenglishsad" would return true while "asdneglihsth" would return false. Upper and lower case does not matter. Return values should be booleans (true/false).
+//2. Given a string of any length with any characters in it, write a function to determine whether or not the string contains the whole word "english". The order of characters/spelling is important, a string "agkjnenglishsad" would return true while "asdneglihsth" would return false. Upper and lower case does not matter. Return values should be booleans (true/false).
 
 function hasEnglish(sequence) {
     let ourString = "english";
@@ -40,7 +40,7 @@ console.log(hasEnglish("SMFENgliSHasnD")); // Input: "SMFENgliSHasnD" | Output: 
 
 
 
-// Given an array of integers, write a function that returns an array of 2 integers. The first integer will be the count of the positive integers, and the second integer will be the sum of the negative integers. If the input is empty or null, return an empty array.
+//3.  Given an array of integers, write a function that returns an array of 2 integers. The first integer will be the count of the positive integers, and the second integer will be the sum of the negative integers. If the input is empty or null, return an empty array.
 
 function posAndNeg(sequence) {
     if (sequence === [] || sequence === null) {
@@ -68,7 +68,7 @@ function posAndNeg(sequence) {
 // Input: null | Output: []
 
 
-// Write a function that translates an input string by replacing each character with a number representing the amount of times that character appears in the string. Then separate each individual number with a different character.
+// 4. Write a function that translates an input string by replacing each character with a number representing the amount of times that character appears in the string. Then separate each individual number with a different character.
 //
 //     Examples:
 //
